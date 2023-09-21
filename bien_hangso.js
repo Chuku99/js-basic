615bf238bc91a5251658da514d6267f832043bf4
Chukku99 <thanhtruc30699@gmail.com>
2023-09-21 20:25:57 +0700

    a

--- bien_hangso.js
+++ bien_hangso.js
@@ -1,1 +1,120 @@
-console.log("Hello world!")
+// console.log("Hello world")
+
+// //Bai 1
+// //cau a
+// console.log("Hello world")
+// //cau b
+// console.log("Tran Thanh Truc")
+// //cau c
+// // console.clear()
+// //cau d
+// console.log("*****")
+// console.log("*****")
+// console.log("*****")
+// console.log("*****")
+// console.log("*****")
+
+// //cau e
+// console.log("*")
+// console.log("***")
+// console.log("*****")
+// console.log("*******")
+// console.log("*********")
+// console.log("************")
+
+// //cau f
+// console.log(" ** ** ")
+// console.log("*  *  *")
+// console.log("*     *")
+// console.log(" *   * ")
+// console.log("  * *  ")
+// console.log("   *   ")
+
+// // bai2
+
+// // cau a
+// let a = 10
+// console.log(a)
+
+// // cau b
+// let b = "10"
+// console.log(b)
+
+// // cau d
+// const pi = 5
+// console.log(pi)
+
+// // cau f
+// let ten = "iPhone 14"
+// let hangSanXuat = "Apple"
+// let mauSac = "Trang"
+// let gia = "1000usd"
+// console.log(ten, hangSanXuat, mauSac, gia)
+
+// bai 3
+
+// cau a
+let a = 2
+let b = 4 
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+
+// cau b
+console.log((a+b)/2)
+
+// cau c
+let x = 3
+let y = 4
+let z = 6
+console.log(x**2+2*x+1)
+console.log(x**3-3*x*y-5*y+3*y**2)
+console.log((x*y)**2-2*x**2*y+13*y)
+console.log(4*x**3+3*x*y+y**2-(2*x**2-3*y))
+console.log(5*x**2/4*x*y+y**2)
+console.log(x**2-2*z*x*y/y**2+5*x-2*y**2+4*x*z**3+z**3)
+
+// cau d
+let duongKinh = 5
+let chuVi = duongKinh*3.14
+let dienTich = 3.14*(duongKinh/2)**2
+console.log(chuVi, dienTich)
+
+// cau e
+let dai = 15
+let rong = 10
+let chuvi = (dai + rong)*2
+let dientich = dai*rong
+console.log(chuvi, dientich)
+
+// cau f
+let S = 120
+let t = 130
+let V = S/(t/60)
+console.log(V)
+
+// cau g
+let tgian = 150
+let vantoc = 60
+let quangduong = vantoc*tgian/60
+console.log(quangduong)
+
+// cau h
+let vtoc = 15.2
+let tg = 3
+let qd = vtoc*tg
+console.log(qd)
+
+// cau i
+let time = 2.4
+let vt = 42
+let ab = time*vt
+console.log(ab)
+
+// cau j
+console.log(8*5*6+8*3*5*6)
+
+
+
+
