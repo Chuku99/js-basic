28010f42dd16d883617070bcb7aa8f419443ada8
Chuku99 <thanhtruc30699@gmail.com>
2023-09-27 17:42:48 +0700

    a

--- function.js
+++ function.js
@@ -6,22 +6,90 @@ function tinhTong(a,b) {
 const kq1 = tinhTong(1,2)
 console.log(kq1)
 
+
 function tinhHieu (a,b) {
     const ketQua = a- b
 
     return ketQua
 }
-
-const kq2 = 10-9
+const kq2 = tinhHieu(10,12)
 console.log(kq2)
+
 
 function tinhNhan (a,b) {
     const ketQua = a * b
 
     return ketQua
 }
-
-const kq3 = 11*22
+const kq3 = tinhNhan(11,12)
 console.log(kq3)
 
+
+function tinhChia (a,b) {
+    const ketQua = a / b
+
+    return ketQua
+}
+const kq4 = tinhChia(10,5)
+console.log(kq4)
+
+function trungBinhCong (a,b){
+    const result = (a+b)/2
+    return result
+}
+const kq5 = trungBinhCong(10,5)
+console.log(kq5)
 // ABDEF
+
+function chuViHinhTron(d) {
+    const result = d*3.14
+    return result
+}
+let cvht = chuViHinhTron(5)
+console.log(cvht)
+
+function dienTichHinhTron (r) {
+    const result = 2*r*3.14
+    return result
+}
+let dtht = dienTichHinhTron(2.5)
+console.log(dtht)
+
+function chuViHinhChuNhat(dai,rong) {
+    const result = (dai+rong)*2
+    return result
+}
+const cvhcn = chuViHinhChuNhat(15,10)
+console.log(cvhcn)
+
+
+function dienTichHCN(dai,rong) {
+    const result = dai*rong
+    return result
+}
+const dthcn = dienTichHCN(15,10)
+console.log(dthcn)
+
+function vanToc(S,t) {
+    const result = S/t
+    return result
+}
+const van_toc = vanToc(120,130/60)
+console.log(van_toc)
+
+function namNhuan(year) {
+    const result = year%4
+    return result
+}
+if result {
+    console.log("nam nhuan", namNhuan)
+}else {
+    console.log("Nam k nhuan", namNhuan)
+}
+let year = 1004
+
+
+
+
+
+
